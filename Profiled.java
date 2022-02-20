599b0cb148414a8f6ec2dd256b50dd2ee738afca
Michael Doherty <60930816+MichaelD58@users.noreply.github.com>
2022-02-20 04:10:34 +0000

    Add files via upload

--- Profiled.java
+++ Profiled.java
@@ -2,9 +2,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
-class GShare {
+class Profiled {
 
     final static DecimalFormat df = new DecimalFormat("0.00");
     public static void main(String [] args){
@@ -24,7 +25,7 @@ class GShare {
                 i *= 2;
             }
         }else{
-            System.out.println("Proper Usage: java GShare <individual file or directory of files> <table size>");
+            System.out.println("Proper Usage: java Profiled <individual file or directory of files> <table size>");
             System.exit(0);
         }
 
@@ -65,20 +66,49 @@ class GShare {
         //https://stackoverflow.com/questions/23751618/how-to-split-a-java-string-at-backslash
         filenameArray = filename.split("\\\\");
         if(taken.size() > 0){
-            System.out.println(filenameArray[2] + ": " + df.format(gShare(branch, taken, tableSize)) + "%");
+            System.out.println(filenameArray[2] + ": " + df.format(profiled(branch, taken, tableSize)) + "%");
         }
         
     }
 
-    static double gShare(ArrayList<String> branch, ArrayList<Integer> taken, int tableSize){
+    static double profiled(ArrayList<String> branch, ArrayList<Integer> taken, int tableSize){
         int correct = 0;
         int sigBits = (int)(Math.log(tableSize) / Math.log(2));
-        String[] buffer = new String[tableSize];
-        String global = "0";
-
-        for(int i = 0; i < taken.size(); i++){
+        HashMap<String, Integer> addressCounter = new HashMap<String, Integer>();
+        
+        for(int i = 0; i < branch.size(); i++){
             String binary = Long.toBinaryString(Long.parseLong(branch.get(i)));
+
+            if (binary.length() > sigBits) {
+                binary = binary.substring(binary.length() - sigBits);
+            }else{
+                binary = branch.get(i);
+            }
             
+            if(addressCounter.size() < tableSize){
+                if(!addressCounter.containsKey(binary)){
+                    if(taken.get(i) == 0){
+                        addressCounter.put(binary, 0);
+                    }else{
+                        addressCounter.put(binary, 1);
+                    }
+                }else{
+                    if(taken.get(i) == 0){
+                        addressCounter.put(binary, addressCounter.get(binary) - 1);
+                    }else{
+                        addressCounter.put(binary, addressCounter.get(binary) + 1);
+                    }
+                }
+            }else if(addressCounter.size() == tableSize){
+                break;
+            }
+            
+   
+        }
+
+
+        for(int i = 0; i < branch.size(); i++){
+            String binary = Long.toBinaryString(Long.parseLong(branch.get(i)));
 
             if (binary.length() > sigBits) {
                 binary = binary.substring(binary.length() - sigBits);
@@ -86,46 +116,22 @@ class GShare {
                 binary = branch.get(i);
             }
 
-            //if (global.length() > sigBits) {
-                global = global.substring(global.length() - 1);
-            //}
-
-            int a = Integer.parseInt(binary, 2);
-            int b = Integer.parseInt(global, 2);
-            int index = a ^ b;
-
-            if(buffer[index] == null){
-                buffer[index] = "00";
+            if(!addressCounter.containsKey(binary)){
+                if(taken.get(i) == 1){
+                    correct++;
+                }
+            }else{            
+                if(addressCounter.get(binary) > 0){
+                    if(taken.get(i) == 1){
+                        correct++;
+                    }
+                }else{
+                    if(taken.get(i) == 0){
+                        correct++;
+                    }
+                }   
             }
             
-            if(buffer[index].equals("00")){
-                if(taken.get(i) == 1){
-                    buffer[index] = "01";
-                }else{
-                    correct++;
-                }
-            }else if(buffer[index].equals("01")){
-                if(taken.get(i) == 1){
-                    buffer[index] = "11";
-                }else if(taken.get(i) == 0){
-                    buffer[index] = "00";
-                    correct++;
-                } 
-            }else if(buffer[index].equals("11")){
-                if(taken.get(i) == 0){
-                    buffer[index] = "10";
-                }else{
-                    correct++;
-                }
-            }else{
-                if(taken.get(i) == 1){
-                    buffer[index] = "11";
-                    correct++;
-                }else if(taken.get(i) == 0){
-                    buffer[index] = "00";
-                }
-            }
-            global += taken.get(i);
         }
 
         double difference = ((double) (taken.size() - correct) / taken.size()) * 100;
